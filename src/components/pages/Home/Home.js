15cfcf20d2d8c0b42a15d26fc341f550be0c2e79
Vivi Cowan <vivicare@gmail.com>
2021-09-02 23:53:25 -0500

    successful responsive logo

---
+++ src/components/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import Logo from "./home-logo.png";
+
+const Home = () => {
+  return (
+    <div className="intro">
+      <img className="logo" src={Logo} alt="Vivianna Cowan"></img>
+      <h1>Test Test</h1>
+    </div>
+  );
+};
+
+export default Home;
