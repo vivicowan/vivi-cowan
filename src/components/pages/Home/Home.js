3d8c79835576dd99922acd5fdd9ecc0a58a66638
Vivi Cowan <vivicare@gmail.com>
2021-09-04 19:04:58 -0500

    adding a greeting for the landing page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,13 +1,55 @@
 import React from "react";
 import "./Home.css";
-import Logo from "./home-logo.png";
+import { Fade } from "react-reveal";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   return (
-    <div className="intro">
-      <img className="logo" src={Logo} alt="Vivianna Cowan"></img>
-      <h1>Test Test</h1>
-    </div>
+    <Fade bottom duration={2000} distance="40px">
+      <div className="greet-main" id="greeting">
+        <div className="greeting-main">
+          <div className="greeting-text-div">
+            <div>
+              <h1 className="greeting-text">Hey there!</h1>
+              <h1 className="greeting-sub">
+                I'm <mark data-entity="person">Vivianna Cowan</mark>
+              </h1>
+              <h1 className="greeting-typewriter">
+                <Typewriter
+                  onInit={(typewriter) => {
+                    typewriter
+                      .typeString("Full-Stack Developer")
+                      .pauseFor(200)
+                      .deleteAll()
+                      .typeString("Software Engineer")
+                      .pauseFor(200)
+                      .deleteAll()
+                      .typeString("UI/UX Designer")
+                      .pauseFor(200)
+                      .deleteAll()
+                      .start();
+                  }}
+                  options={{
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
+              </h1>
+              <span className="greeting-text-span subTitle">
+                <div className="entities">
+                  A <mark data-entity="noun">Data Science practitioner</mark>{" "}
+                  who thrives to{" "}
+                  <mark data-entity="verb">leverage Startups</mark> <br /> using
+                  AI-based solutions along with{" "}
+                  <mark data-entity="skill">Web Development</mark> <br /> and a
+                  blend of <mark data-entity="freelance">UI/UX Designing</mark>
+                </div>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fade>
   );
 };
 
