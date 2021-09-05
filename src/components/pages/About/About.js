f78cd31ee22af9ee18e5a3272973b1b302920965
Vivi Cowan <vivicare@gmail.com>
2021-09-05 14:53:22 -0500

    made a grid layout for about and work page

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,11 +1,42 @@
 import React from "react";
 import "./About.css";
+import { Fade } from "react-reveal";
+import Typewriter from "typewriter-effect";
 
 const About = () => {
   return (
-    <div>
-      <h1>Test Test</h1>
-    </div>
+    <Fade>
+      <div id="wrapper2">
+        <div id="one2"></div>
+        <div id="two2">
+          <h1 className="greeting-typewriter2">
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString("Who is Vivi?")
+                  .pauseFor(200)
+                  .deleteAll()
+                  .start();
+              }}
+              options={{
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h1>
+        </div>
+
+        <div id="three2">3</div>
+
+        <div id="eleven2"></div>
+        <div id="twelve2">12</div>
+        <div id="thirteen2">
+          <div className="pixel-animation"></div>
+        </div>
+
+        <div id="sixteen2">16</div>
+      </div>
+    </Fade>
   );
 };
 
--- src/components/pages/Work/Work.js
+++ src/components/pages/Work/Work.js
@@ -3,8 +3,22 @@ import "./Work.css";
 
 const Work = () => {
   return (
-    <div>
-      <h1>Test Test</h1>
+    <div id="wrapper1">
+      <div id="two1">2</div>
+      <div id="three1">3</div>
+      <div id="four1">4</div>
+      <div id="five1">5</div>
+      <div id="six1">6</div>
+      <div id="seven1">7</div>
+      <div id="eight1">8</div>
+      <div id="nine1">9</div>
+      <div id="ten1">10</div>
+      <div id="eleven1">11</div>
+      <div id="twelve1">12</div>
+      <div id="thirteen1">13</div>
+      <div id="fourteen1">14</div>
+      <div id="fifteen1">15</div>
+      <div id="sixteen1">16</div>
     </div>
   );
 };
