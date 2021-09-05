4c3343a61a0fd854b85fef528840c8faa0acdc3f
Vivi Cowan <vivicare@gmail.com>
2021-09-05 17:19:03 -0500

    created the skills section in about page

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./About.css";
 import { Fade } from "react-reveal";
 import Typewriter from "typewriter-effect";
+import { IconContext } from "react-icons";
+import { FaCode, FaSearch, FaBolt } from "react-icons/fa";
 
 const About = () => {
   return (
     <Fade>
       <div id="wrapper2">
-        <div id="one2"></div>
+        <div id="one2">About</div>
         <div id="two2">
           <h1 className="greeting-typewriter2">
             <Typewriter
@@ -25,8 +27,25 @@ const About = () => {
             />
           </h1>
         </div>
-
-        <div id="three2">3</div>
+        <div id="three2">
+          <div className="skill-box">
+            <h3 className="skills">Skills:</h3>
+            <div className="icons">
+              <div className="one">
+                <FaBolt />
+                <p>Creativity</p>
+              </div>
+              <div className="two">
+                <FaCode />
+                <p>Web Development</p>
+              </div>
+              <div className="three">
+                <FaSearch />
+                <p>Research</p>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <div id="eleven2"></div>
         <div id="twelve2">12</div>
@@ -34,7 +53,7 @@ const About = () => {
           <div className="pixel-animation"></div>
         </div>
 
-        <div id="sixteen2">16</div>
+        <div id="sixteen2">Education</div>
       </div>
     </Fade>
   );
