0bd54d4cbd6bd4fb5410a79a51f951817c527f60
Vivi Cowan <vivicare@gmail.com>
2021-09-14 20:32:53 -0500

    added buttons that downloads resume

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,15 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./About.css";
 import { Fade } from "react-reveal";
 import Typewriter from "typewriter-effect";
-import { IconContext } from "react-icons";
 import { FaCode, FaSearch, FaBolt } from "react-icons/fa";
+import Resume from "../../../content/VC-Technical-Resume.pdf";
 
-const About = () => {
+const About = (props) => {
   return (
     <Fade>
       <div id="wrapper2">
-        <div id="one2">About</div>
+        <div id="one2">
+          <div className="about-box">
+            <h3 className="about-me">About Me:</h3>
+            <div className="about-section">
+              <p>
+                I am a full-time student from San Diego. Currently a transfer
+                student at the University of Texas in Arlington earning my
+                degree in Software Engineering. I enjoy learning new things and
+                take every challenge head-on. I have an eye for detail and a
+                vivid imagination. I am very interested in the world of
+                Full-Stack Web Development and UI/UX Designing.
+              </p>
+            </div>
+          </div>
+        </div>
         <div id="two2">
           <h1 className="greeting-typewriter2">
             <Typewriter
@@ -32,28 +47,60 @@ const About = () => {
             <h3 className="skills">Skills:</h3>
             <div className="icons">
               <div className="one">
-                <FaBolt />
+                <FaBolt size="2em" />
                 <p>Creativity</p>
               </div>
               <div className="two">
-                <FaCode />
+                <FaCode size="2em" />
                 <p>Web Development</p>
               </div>
               <div className="three">
-                <FaSearch />
+                <FaSearch size="2em" />
                 <p>Research</p>
               </div>
             </div>
           </div>
         </div>
 
-        <div id="eleven2"></div>
-        <div id="twelve2">12</div>
+        <div id="eleven2">
+          <div className="buttons">
+            <span className="click">
+              <a className="click-btn" href="/contact">
+                <Link to="/contact">Contact</Link>
+              </a>
+            </span>
+            <span className="click">
+              <a className="click-btn" href={Resume} download>
+                Download Resume
+              </a>
+            </span>
+          </div>
+        </div>
         <div id="thirteen2">
           <div className="pixel-animation"></div>
         </div>
 
-        <div id="sixteen2">Education</div>
+        <div id="sixteen2">
+          {" "}
+          <div className="school-box">
+            <h3 className="school">Education:</h3>
+            <div className="school-section">
+              <p>
+                University of Texas at Arlington: <br />
+                <hr className="hr" />
+                Granted Honors Scholarship <br />
+                Pursuing Bachelor of Science in Software Engineering (BSSE) - In
+                progress <br />
+              </p>
+              <p>
+                University of California, San Diego: <br />
+                <hr className="hr" />
+                Extension Online <br />
+                San Diego Coding Bootcamp - Complete
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </Fade>
   );
